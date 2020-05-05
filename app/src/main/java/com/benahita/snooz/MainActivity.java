728efc48e98619575658023c1ae8137f9f16f0c5
yvonbenahita <yvonbenahita@gmail.com>
2020-05-05 23:00:08 +0300

    Les dots indicateurs du slide

--- app/src/main/java/com/benahita/snooz/MainActivity.java
+++ app/src/main/java/com/benahita/snooz/MainActivity.java
@@ -5,6 +5,7 @@ import android.Manifest;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
@@ -12,6 +13,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,6 +25,11 @@ public class MainActivity extends AppCompatActivity {
     // Btn close
     private TextView mCloseBtn;
 
+    //Page indicator du slider
+    LinearLayout sliderDotspanel;
+    private int dotscount;
+    private ImageView[] dots;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,9 +38,53 @@ public class MainActivity extends AppCompatActivity {
         // Btn close
         TextView textViewClose = (TextView) findViewById(R.id.activity_main_close_btn);
 
-        // slide
+        // =================================== slide ======================
         ViewPager viewPager = (ViewPager) findViewById(R.id.activity_main_viewpager);
         viewPager.setAdapter(new CustomPagerAdapter(this));
+        sliderDotspanel = (LinearLayout) findViewById(R.id.activity_main_slider_dots); // Les dots
+        dotscount = new CustomPagerAdapter(this).getCount();
+        dots = new ImageView[dotscount];
+
+        for(int i = 0; i < dotscount; i++){
+
+            dots[i] = new ImageView(this);
+            dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.nonactive_dot));
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            params.setMargins(8, 0, 8, 0);
+
+            sliderDotspanel.addView(dots[i], params);
+
+        }
+
+        dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                for(int i = 0; i< dotscount; i++){
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.nonactive_dot));
+                }
+
+                dots[position].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        // ================================Fin slide ================================
+
 
         // Premier ouverture de lapp
         SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
