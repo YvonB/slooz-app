e4f317251c769ca67d75ccdf8e2882e5dd2a8e6f
yvonbenahita <yvonbenahita@gmail.com>
2020-08-22 16:32:45 +0300

    Tentative de Unkillable 1

--- app/src/main/java/com/benahita/slooz/MyService.java
+++ app/src/main/java/com/benahita/slooz/MyService.java
@@ -1,13 +1,18 @@
 package com.benahita.slooz;
 
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 public class MyService extends Service {
+
+    private static final int NOTIF_ID = 1;
+    private static final String NOTIF_CHANNEL_ID = "Channel_Id";
 
     @Nullable
     @Override
@@ -17,13 +22,15 @@ public class MyService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId){
+
         Toast.makeText(MyService.this, "Commence en arrière plan !", Toast.LENGTH_LONG).show();
 
-        //return super.onStartCommand(intent, flags, startId);
-        return START_REDELIVER_INTENT;
+        startForeground();
+        return super.onStartCommand(intent, flags, startId);
+        //return START_REDELIVER_INTENT;
     }
 
-    /*private void startForeground() {
+    private void startForeground() {
         Intent notificationIntent = new Intent(this, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
                 notificationIntent, 0);
@@ -32,8 +39,8 @@ public class MyService extends Service {
                 .setOngoing(true)
                 .setSmallIcon(R.drawable.ic_launcher_background)
                 .setContentTitle(getString(R.string.app_name))
-                .setContentText("Snooz fonctionne en arrière-plan")
+                .setContentText("Slooz fonctionne en arrière-plan")
                 .setContentIntent(pendingIntent)
                 .build());
-    }*/
+    }
 }
